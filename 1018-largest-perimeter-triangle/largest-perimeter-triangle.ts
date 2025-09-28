1fd6f126656ec374ac7eeb795752c86662fa8c06
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2025-09-28 12:58:57 +0530

    Time: 26 ms (50.00%) | Memory: 59.3 MB (48.21%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.ts
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.ts
@@ -1,12 +1,12 @@
 function largestPerimeter(nums: number[]): number {
-   nums.sort((a,b)=> a-b);
+    nums.sort((a, b) => a - b);
 
-   for(let i=nums.length-1;i>1;i--){
-         let a = nums[i];
-         let b = nums[i-1];
-         let c = nums[i-2];
+    for (let i = nums.length - 1; i > 1; i--) {
+        let a = nums[i];
+        let b = nums[i - 1];
+        let c = nums[i - 2];
 
-         if(a < b+c) return a+b+c;
-   }
-   return 0;
+        if (a < b + c) return a + b + c;
+    }
+    return 0;
 };
